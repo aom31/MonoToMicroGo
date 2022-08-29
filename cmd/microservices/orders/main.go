eacad44e8c67894f62317b2891fc088d89ce7dd0
Thamakorn Ketnoi <thamakorn.k@siriustech.io>
2022-08-29 16:46:55 +0700

    add microservice

--- cmd/microservices/orders/main.go
+++ cmd/microservices/orders/main.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	log.Println("Starting order microservice")
+
+	ctx := cmd.Context()
+
+	r, closeFn := createOrderMicroservices()
+
+	defer closeFn()
+
+	server := &http.Server{Addr: os.Getenv("SHOP_ORDER_SERVICE_BIND_ADDR"), Handler: r}
+
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			panic(err)
+		}
+	}()
+	<-ctx.Done()
+	log.Println("closing order microservice")
+
+	if err := server.Close(); err != nil {
+		panic(err)
+	}
+
+}
+
+func createOrderMicroservices() (router *chi.Mux, closeFn func()) {
+	cmd.WaitForService(os.Getenv("SHOP_RABBITMQ_ADDR"))
+
+	shopHTTPClient := orders.infra_product.NewHTTPClient(os.Getenv("SHOP_SERVICE_ADDR"))
+
+	r := cmd.CreateRouter()
+
+	orders_public_http.AddRoutes(r, ordersService, ordersRepo)
+	orders_private_http.AddRoutes(r, ordersService, ordersRepo)
+
+	return r, func() {
+
+	}
+}
--- cmd/microservices/shop/main.go
+++ cmd/microservices/shop/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Println("Starting shop microservice")
+
+	ctx := cmd.Context()
+
+	r := createShopMicroservice()
+	server := &http.Server{Addr : os.Getenv("SHOP_PRODUCT_SERVICE_BIND_ADDR"), Handler: r}
+	go func (){
+		if err := server.ListenAndServe(); err != http.ErrServerClosed{
+			panic(err)
+		}
+	}()
+		<= ctx.Done()
+		log.Println("Closing shop microservice")
+		if err := server.Close(); err != nil {
+			panic(err)
+		}
+	
+}
+func createShopMicroservice() *chi.Mux{
+	shopProductRepo := shop_infra_prodcut.NewMemoryRepository()
+
+	r:= cmd.CreateRouter()
+
+	shop_interfaces_public_http.AddRoutes(r, shopProductRepo)
+	shop_interfaces_private_http.AddRoutes(r, shopProductRepo)
+
+	return r 
+}
